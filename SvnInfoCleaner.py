a655b9b0496a6b417baef24e0ee8664c845d930b
jhezjkp <jhezjkp@gmail.com>
2012-04-23 22:13:18 +0800

    解决windows下因目录只读属性导致删除失败的情况

--- SvnInfoCleaner.py
+++ SvnInfoCleaner.py
@@ -5,12 +5,23 @@
 该脚本用于清理脚本所在目录下SVN信息
 '''
 
-import os, shutil
+import sys, os, shutil, stat
+
+def on_rm_error(func, path, exc_info):
+    '''
+    解决windows下因目录只读属性导致删除失败的情况
+    '''
+    os.chmod(path, stat.S_IWRITE)
+    os.unlink(path)
 
 for root, dirs, files in os.walk("."):
     for dir in dirs:
-        if dir==".svn":
+        if dir==".svn":            
             path = os.path.join(root, dir)
             print "deleting", path
-            shutil.rmtree(path)
+            try:
+                shutil.rmtree(path, onerror=on_rm_error)
+            except:
+                info = sys.exc_info()
+                print info[0], ":", info[1]
             
