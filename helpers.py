7ea83ec75b4aab00a1b756b7fc9979e71b09b74e
jhezjkp <jhezjkp@gmail.com>
2012-04-30 15:42:51 +0800

    通用辅助类

---
+++ helpers.py
@@ -0,0 +1,27 @@
+#!/bin/python
+#encoding=utf-8
+
+'''
+一个通用的辅助工具类
+'''
+
+import sys
+
+def hilite(msg, status, bold):
+    if sys.stdout.isatty():
+        attrs = []
+        if status:
+            #绿色
+            attrs.append('32')
+        else:
+            #红色
+            attrs.append('31')
+        if bold:
+            attrs.append('1')
+        return '\x1b[%sm%s\x1b[0m' % (';'.join(attrs), msg)
+    else:
+        return msg
+
+if __name__=='__main__':
+    print hilite('helpers test', True, True)
+    
