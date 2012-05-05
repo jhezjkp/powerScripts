de332528f75d83cf21e16fa33cf2908e80e48601
jhezjkp <jhezjkp@gmail.com>
2012-05-05 15:12:29 +0800

    增加文件哈希计算方法

--- helpers.py
+++ helpers.py
@@ -6,6 +6,9 @@
 '''
 
 import sys
+import os.path
+import hashlib
+
 from colorama import Fore, Back, Style, init
 
 #init colorama
@@ -18,6 +21,44 @@ def hilite(msg, foreColor=Fore.WHITE, backColor=Back.GREEN, style=Style.BRIGHT):
     else:
         return msg
 
+class DirHashUnavaliableException(Exception):
+    '''目录哈希值计算不可用异常(暂不支持目录的哈希计算)'''
+    def __init__(self):
+        Exception.__init__(self)
+
+    def __str__(self):
+        return "暂时不支持对目录的希值计算"
+
+
+def hashFile(filePath, algorithm='md5'):
+    '''计算文件哈希值，默认为md5算法'''
+    if not os.path.exists(filePath):
+        raise IOError("No such file: '"+filePath+"'") 
+    elif os.path.isdir(filePath):
+        raise DirHashUnavaliableException()
+    h = hashlib.new(algorithm)
+    with open(filePath) as f:
+        for line in f:
+            h.update(line)
+    return h.hexdigest()
+
 if __name__=='__main__':
     print 'hilite:', hilite('customise hilite', Fore.RED, Back.WHITE, Style.DIM), 'and', hilite('default hilite')
+    print "md5("+__file__+")", hashFile(__file__)
+    print "sha1"+__file__+")", hashFile(__file__, 'sha1')
+    print "sha224("+__file__+")", hashFile(__file__, 'sha224')
+    print "sha256("+__file__+")", hashFile(__file__, 'sha256')
+    print "sha384("+__file__+")", hashFile(__file__, 'sha384')
+    print "sha512("+__file__+")", hashFile(__file__, 'sha512')
+    try:
+        hashFile('/tmp')
+    except:
+        info = sys.exc_info()
+        print info[0], ":", info[1]
+    try:
+        hashFile('/nonexist/file')
+    except:
+        info = sys.exc_info()
+        print info[0], ":", info[1]
+
     
