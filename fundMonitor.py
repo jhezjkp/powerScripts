9d11560ba13ce339e161b13152e33544de988fa3
jhezjkp <jhezjkp@163.com>
2013-06-30 16:02:29 +0800

    基金监控程序

--- fundMonitor.py
+++ fundMonitor.py
@@ -10,9 +10,23 @@ import sys
 import os
 import os.path
 import urllib
+import re
 import requests
 from BeautifulSoup import BeautifulSoup
 
+#通知邮件地址
+email = "fund@vivia.me.mailgun.org"
+
+def send_simple_message(content):
+    result = requests.post(
+        "https://api.mailgun.net/v2/vivia.me.mailgun.org/messages",
+        auth=("api", "key-9lo3lwpmkddrw32r55pcw2dh49prpn00"),
+        data={"from": "基金风险监控 <postmaster@vivia.me.mailgun.org>",
+              "to": [email],
+              "subject": "基金风险报告",
+              "text": content})
+    print "mail sent!"
+    return result
 
 if __name__ == '__main__':
     reload(sys)
@@ -20,10 +34,22 @@ if __name__ == '__main__':
     pageUrl = "http://fund.eastmoney.com/000198.html"
     r = requests.get(pageUrl)
     if r.status_code == 200:
-    	print unicode(r.text)
-        soup = BeautifulSoup(r.text, fromEncoding="gb18030")
-        print soup.originalEncoding
+    	#指定解析编码
+    	r.encoding = "gb18030"
+        soup = BeautifulSoup(r.text)
         fundTable = soup.find("table", {"class": "sytable"}).findAll("td")
-        print unicode(fundTable[0].text)
-        print fundTable[0].text
-
+        #万份收益率
+        _10kProfit = fundTable[3].text
+        _10kProfit = float(_10kProfit[0:_10kProfit.find("&nbsp;")])
+        #七日年化收益
+        _7DayYearlyProfit = float(fundTable[7].text.replace("%", ""))
+        #基金经理
+        fundManager = fundTable[19].text.replace("&nbsp;", "").strip()
+        print _10kProfit, _7DayYearlyProfit, fundManager
+        if fundManager != "王登峰" or _7DayYearlyProfit < 3 or _10kProfit < 1:
+        	print "warning..."
+        	content = "基金经理：" + fundManager + "\n"
+        	content += "万份收益：" + str(_10kProfit) + "元\n"
+        	content += "7日年化收效率" + str(_7DayYearlyProfit) + "%\n"
+        	send_simple_message(content)
+        
