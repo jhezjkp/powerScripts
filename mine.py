6516dba03a7fa2797263b64231b2dc6b0e47c161
vivia <jhezjkp@163.com>
2015-09-16 10:19:27 +0800

    blank area detect

--- mine.py
+++ mine.py
@@ -18,6 +18,8 @@ mine_count = 0
 grid = [([0] * width) for i in range(height)]
 #marked as blank grid
 marked = []
+#auto blank grid
+auto_blank = []
 
 
 def display_grid(mode=0):
@@ -26,7 +28,7 @@ def display_grid(mode=0):
 			if mode == 0:
 				print grid[i][j], "\t",
 			elif mode == 1:
-				if (i * width + j) in marked:
+				if (i * width + j) in marked or (i * width + j) in auto_blank:
 					print get_label(i, j), "\t",
 				else:
 					print "#", "\t",
@@ -69,6 +71,23 @@ def get_label(x, y):
 	return label
 
 
+def auto_blank_check(x, y):
+	label = get_label(x, y)
+	#print "check ", x, y, label
+	if label == 0:
+		for i in [-1, 0, 1]:
+			for j in [-1, 0, 1]:
+				m = x + i
+				n = y + j
+				if m < 0 or n <0 or m > (height - 1) or n > (width - 1)  or (m == x and n == y) or (m * width + n) in auto_blank:
+					continue
+				elif get_label(m, n) == 0:
+					auto_blank.append(m * width + n)
+					auto_blank_check(m, n)
+				else:
+					auto_blank.append(m * width + n)
+
+
 def mark_mine():
 	mark = int(raw_input("Plear mark a blank grid(0~" + str(width * height - 1) + "): "))
 	if mark in marked:
@@ -83,7 +102,8 @@ def mark_mine():
 		print "game over"
 		print "--------------------"
 		display_grid(2)
-	elif width * height - len(marked) != mine_count:
+	elif width * height - len(marked) - len(auto_blank) != mine_count:
+		auto_blank_check(mark / width, mark % width)
 		print "----------------"
 		print marked
 		display_grid(1)
